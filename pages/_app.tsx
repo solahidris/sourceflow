7dfc2eaab1f8d8defb9f757022cf57bd73421598
solahidris <emailsolah@gmail.com>
2024-03-09 00:03:37 +0800

    installed bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min');
+  }, []);
+  
   return <Component {...pageProps} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,44 +6,60 @@ export default function Home() {
     <main className="min-h-screen bg-white text-black">
       
       {/* Header */}
-      <div className="h-[84px] flex justify-between py-[20px] px-[120px] items-center">
+      <div className="d-flex justify-content-between align-items-center navbar-div-style">
         {/* Frame 8 */}
-        <div className="flex items-center gap-[46px]"> 
+        <div className="d-flex align-items-center gap-[46px]">
           {/* Logo */}
           <div>
-            <span className="text-[20px] font-[700] text-[#064EA4]">Software Recruitment co.</span>
+            <span className="navbar-brand-text fw-bold">Software Recruitment co.</span>
           </div>
           {/* Menu */}
           <div>
             {/* Frame 7 */}
-            <div className="flex gap-[30px] text-[16px] font-[400]">
+            <div className="flex gap-[30px]">
               {/* Frame 1 */}
-              <div>
-                <span>For jobseekers</span>
-                <span> &#x25BF;</span>
+              <div className="dropdown">
+                <button className="btn dropdown-toggle navbar-dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  For jobseekers
+                </button>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">new page</a></li>
+                </ul>
               </div>
               {/* Frame 2 */}
-              <div>
-                <span>For clients</span>
-                <span> &#x25BF;</span>
+              <div className="dropdown">
+                <button className="btn dropdown-toggle navbar-dropdown-toggle" custom-dropdown-toggle type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  For clients
+                </button>
+                <ul className="dropdown-menu ">
+                  <li><a className="dropdown-item" href="#">new page</a></li>
+                </ul>
               </div>
               {/* Frame 3 */}
-              <div>
-                <span>Sectors</span>
-                <span> &#x25BF;</span>
+              <div className="dropdown">
+                <button className="btn dropdown-toggle navbar-dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Sectors
+                </button>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">new page</a></li>
+                </ul>
               </div>
               {/* Frame 4 */}
-              <div>
-                <span>Resources</span>
-                <span> &#x25BF;</span>
+              <div className="dropdown">
+                <button className="btn dropdown-toggle navbar-dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Resources
+                </button>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">new page</a></li>
+                </ul>
               </div>
             </div>
           </div>
         </div>
         {/* Frame 5 */}
         <div className="flex gap-[10px]">
-          <button className="bg-[#FCDF69] rounded-[30px] py-[12px] px-[20px]">Upload CV</button>
-          <button className="bg-[#F99D76] rounded-[30px] py-[12px] px-[20px]">Contact us</button>
+          <button className="btn navbar-right-corner-btn-yellow py-3 px-4">Upload CV</button>
+          <button className="btn navbar-right-corner-btn-orange py-3 px-4">Contact us</button>
         </div>
         
       </div>
@@ -56,7 +72,7 @@ export default function Home() {
           <div className="mt-[157.5px]">
             {/* Frame 11 */}
             <div>
-              <span className="p-[6px] text-[16px] text-white">Software Recruitment Specialists</span>
+              <span className="p-[6px]  text-white">Software Recruitment Specialists</span>
             </div>
             {/* Frame 14 */}
             <div className="gap-[10px]">
@@ -64,9 +80,9 @@ export default function Home() {
               {/* Frame 13 */}
               <div className="flex h-[62px] w-[592px]">
                 {/* Frame 10 */}
-                <input className="w-[433px] rounded-l-[30px] border border-[1px] px-[20px] py-[10px] gap-[10px] bg-white text-[16px] text-[#AAAAAA]" placeholder="E.g. networking"></input>
+                <input className="w-[433px] rounded-l-[30px] border border-[1px] px-[20px] py-[10px] gap-[10px] bg-white  text-[#AAAAAA]" placeholder="E.g. networking"></input>
                 {/* Frame 12 */}
-                <button className="w-[159px] rounded-r-[30px] bg-[#FCDF69] flex justify-center items-center text-[16px] text-[#25210E] font-[700]">Search jobs</button>
+                <button className="w-[159px] rounded-r-[30px] bg-[#FCDF69] flex justify-center items-center  text-[#25210E] font-[700]">Search jobs</button>
               </div>
             </div>
           </div>
@@ -77,7 +93,7 @@ export default function Home() {
       {/* Latest Clients */}
       <div className="h-[234px] gap-[30px] flex flex-col items-center py-[60px] px-[120px]">
         {/* Header */}
-        <span className="text-[16px] text-[#D2D2D2]">Who we work with</span>
+        <span className=" text-[#D2D2D2]">Who we work with</span>
         {/* Logos */}
         <div className="flex justify-evenly w-full">
           <Image className="object-cover w-[150px] h-[60px] bg-slate-300" src="/next.svg" alt="next" width={100} height={100} />
@@ -121,10 +137,10 @@ export default function Home() {
               </div>
               
               {/* Description */}
-              <span className="text-[16px]">Odio mi amet commodo convallis nunc. Tincidunt mauris eu egestas eget in aliquam.</span>
+              <span className="">Odio mi amet commodo convallis nunc. Tincidunt mauris eu egestas eget in aliquam.</span>
 
               {/* Button Primary */}
-              <button className="bg-[#FFFFFF] rounded-[30px] py-[15px] px-[30px] text-[16px]">View this job</button>
+              <button className="bg-[#FFFFFF] rounded-[30px] py-[15px] px-[30px] ">View this job</button>
 
               {/* Date */}
               <span className="text-[#064EA4CC] text-[13px] font-[300]">Posted on 29/08/2023</span>
@@ -156,10 +172,10 @@ export default function Home() {
               </div>
               
               {/* Description */}
-              <span className="text-[16px]">Odio mi amet commodo convallis nunc. Tincidunt mauris eu egestas eget in aliquam.</span>
+              <span className="">Odio mi amet commodo convallis nunc. Tincidunt mauris eu egestas eget in aliquam.</span>
 
               {/* Button Primary */}
-              <button className="bg-[#FFFFFF] rounded-[30px] py-[15px] px-[30px] text-[16px] text-[#064EA4]">View this job</button>
+              <button className="bg-[#FFFFFF] rounded-[30px] py-[15px] px-[30px]  text-[#064EA4]">View this job</button>
 
               {/* Date */}
               <span className="text-[#FFFFFFCC] text-[13px] font-[300]">Posted on 29/08/2023</span>
@@ -191,10 +207,10 @@ export default function Home() {
               </div>
               
               {/* Description */}
-              <span className="text-[16px]">Odio mi amet commodo convallis nunc. Tincidunt mauris eu egestas eget in aliquam.</span>
+              <span className="">Odio mi amet commodo convallis nunc. Tincidunt mauris eu egestas eget in aliquam.</span>
 
               {/* Button Primary */}
-              <button className="bg-[#FFFFFF] rounded-[30px] py-[15px] px-[30px] text-[16px]">View this job</button>
+              <button className="bg-[#FFFFFF] rounded-[30px] py-[15px] px-[30px] ">View this job</button>
 
               {/* Date */}
               <span className="text-[#064EA4CC] text-[13px] font-[300]">Posted on 29/08/2023</span>
@@ -210,7 +226,7 @@ export default function Home() {
             </div>
             {/* View More Jobs */}
             <div className="p-[10px]">
-              <button className="text-[#0E2152] text-[16px] font-[500]">View more jobs</button>
+              <button className="text-[#0E2152]  font-[500]">View more jobs</button>
             </div>
           </div>
 
@@ -233,7 +249,7 @@ export default function Home() {
 
           <div className="flex flex-col gap-[30px] pr-[30px] w-full">
             <span className="font-[700] text-[33px]">Explore</span>
-            <div className="flex flex-col gap-[20px] font-[400] text-[16px]">
+            <div className="flex flex-col gap-[20px] font-[400] ">
               <Link href="/">Homepage</Link>
               <Link href="/">For jobseekers</Link>
               <Link href="/">For clients</Link>
@@ -245,7 +261,7 @@ export default function Home() {
 
           <div className="flex flex-col gap-[30px] pr-[30px] w-full">
             <span className="font-[700] text-[33px]">Sectors</span>
-            <div className="flex flex-col gap-[20px] font-[400] text-[16px]">
+            <div className="flex flex-col gap-[20px] font-[400] ">
               <Link href="/">Software engineering</Link>
               <Link href="/">DevOps</Link>
               <Link href="/">Cloud</Link>
@@ -257,7 +273,7 @@ export default function Home() {
 
           <div className="flex flex-col gap-[30px] pr-[30px] w-full">
             <span className="font-[700] text-[33px]">Services</span>
-            <div className="flex flex-col gap-[20px] font-[400] text-[16px]">
+            <div className="flex flex-col gap-[20px] font-[400] ">
               <Link href="/">Nav item</Link>
               <Link href="/">Nav item</Link>
               <Link href="/">Nav item</Link>
