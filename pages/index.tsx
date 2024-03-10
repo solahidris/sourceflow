68a4b1c7344a0a3a9cb5c9752f5e2b53bbcfad3d
solahidris <emailsolah@gmail.com>
2024-03-11 01:26:43 +0800

    footer mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -286,28 +286,28 @@ export default function Home() {
 
       {/* Footer */}
       <div className="footer-container-style">
-        <div className="d-flex justify-content-between gap-[24px] w-full">
+        <div className="d-flex justify-content-between lg:gap-[24px] gap-[72px] w-full lg:flex-row flex-col">
 
-          <div className="d-flex flex-column gap-[30px] w-full">
-            <span className="fw-bold text-[19px]">Software Recruitment co.</span>
-            <div className="d-flex gap-[20px]">
-              <div className="w-[22px] h-[24px]">
-                <Link href="/"><svg width="22" height="21" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <div className="d-flex flex-column lg:gap-[30px] gap-[60px] w-full">
+            <span className="fw-bold lg:text-[19px] text-[80px]">Software Recruitment co.</span>
+            <div className="d-flex lg:gap-[20px] gap-[100px]">
+              <div className="lg:w-[22px] lg:h-[24px] w-[88px] h-[96px]">
+                <Link href="/"><svg className="lg:w-[22px] lg:h-[21px] w-[88px] h-[84px]" width="22" height="21" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M4.92446 21.5745H0.363393V7.55423H4.92446V21.5745ZM2.64147 5.64173C1.18299 5.64173 0 4.48861 0 3.09642C1.04392e-08 2.4277 0.278297 1.78637 0.77367 1.31351C1.26904 0.84066 1.94091 0.575012 2.64147 0.575012C3.34204 0.575012 4.0139 0.84066 4.50928 1.31351C5.00465 1.78637 5.28295 2.4277 5.28295 3.09642C5.28295 4.48861 4.09946 5.64173 2.64147 5.64173ZM21.9951 21.5745H17.4438V14.7495C17.4438 13.123 17.4095 11.037 15.0725 11.037C12.7011 11.037 12.3377 12.8042 12.3377 14.6324V21.5745H7.78152V7.55423H12.156V9.46673H12.2198C12.8287 8.36517 14.3162 7.20267 16.5354 7.20267C21.1514 7.20267 22 10.1042 22 13.873V21.5745H21.9951Z" fill="white"/>
                 </svg></Link>
               </div>
-              <div className="w-[24px] h-[24px] p-[2px] d-flex justify-content-center align-items-center">
-                <Link href="/"><svg width="20" height="20" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <div className="lg:w-[24px] lg:h-[24px] w-[96px] h-[96px] p-[2px] d-flex justify-content-center align-items-center">
+                <Link href="/"><svg className="lg:w-[20px] lg:h-[20px] w-[80px] h-[80px]" width="20" height="20" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M20 10.5743C20 5.05434 15.52 0.574341 10 0.574341C4.48 0.574341 0 5.05434 0 10.5743C0 15.4143 3.44 19.4443 8 20.3743V13.5743H6V10.5743H8V8.07434C8 6.14434 9.57 4.57434 11.5 4.57434H14V7.57434H12C11.45 7.57434 11 8.02434 11 8.57434V10.5743H14V13.5743H11V20.5243C16.05 20.0243 20 15.7643 20 10.5743Z" fill="white"/>
                 </svg></Link>
               </div>
-              <div className="w-[24px] h-[24px] p-[2px] d-flex justify-content-center align-items-center">
-                <Link href="/"><svg width="20" height="20" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <div className="lg:w-[24px] lg:h-[24px] w-[96px] h-[96px] p-[2px] d-flex justify-content-center align-items-center">
+                <Link href="/"><svg className="lg:w-[20px] lg:h-[20px] w-[80px] h-[80px]" width="20" height="20" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M5.8 0.574341H14.2C17.4 0.574341 20 3.17434 20 6.37434V14.7743C20 16.3126 19.3889 17.7878 18.3012 18.8756C17.2135 19.9633 15.7383 20.5743 14.2 20.5743H5.8C2.6 20.5743 0 17.9743 0 14.7743V6.37434C0 4.83609 0.61107 3.36083 1.69878 2.27312C2.78649 1.18541 4.26174 0.574341 5.8 0.574341ZM5.6 2.57434C4.64522 2.57434 3.72955 2.95363 3.05442 3.62876C2.37928 4.30389 2 5.21956 2 6.17434V14.9743C2 16.9643 3.61 18.5743 5.6 18.5743H14.4C15.3548 18.5743 16.2705 18.1951 16.9456 17.5199C17.6207 16.8448 18 15.9291 18 14.9743V6.17434C18 4.18434 16.39 2.57434 14.4 2.57434H5.6ZM15.25 4.07434C15.5815 4.07434 15.8995 4.20604 16.1339 4.44046C16.3683 4.67488 16.5 4.99282 16.5 5.32434C16.5 5.65586 16.3683 5.9738 16.1339 6.20822C15.8995 6.44264 15.5815 6.57434 15.25 6.57434C14.9185 6.57434 14.6005 6.44264 14.3661 6.20822C14.1317 5.9738 14 5.65586 14 5.32434C14 4.99282 14.1317 4.67488 14.3661 4.44046C14.6005 4.20604 14.9185 4.07434 15.25 4.07434ZM10 5.57434C11.3261 5.57434 12.5979 6.10112 13.5355 7.03881C14.4732 7.97649 15 9.24826 15 10.5743C15 11.9004 14.4732 13.1722 13.5355 14.1099C12.5979 15.0476 11.3261 15.5743 10 15.5743C8.67392 15.5743 7.40215 15.0476 6.46447 14.1099C5.52678 13.1722 5 11.9004 5 10.5743C5 9.24826 5.52678 7.97649 6.46447 7.03881C7.40215 6.10112 8.67392 5.57434 10 5.57434ZM10 7.57434C9.20435 7.57434 8.44129 7.89041 7.87868 8.45302C7.31607 9.01563 7 9.77869 7 10.5743C7 11.37 7.31607 12.1331 7.87868 12.6957C8.44129 13.2583 9.20435 13.5743 10 13.5743C10.7956 13.5743 11.5587 13.2583 12.1213 12.6957C12.6839 12.1331 13 11.37 13 10.5743C13 9.77869 12.6839 9.01563 12.1213 8.45302C11.5587 7.89041 10.7956 7.57434 10 7.57434Z" fill="white"/>
                 </svg></Link>
               </div>
-              <div className="w-[24px] h-[24px] py-[4px] px-[1.54px] d-flex justify-content-center align-items-center">
-                <Link href="/"><svg width="21" height="17" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <div className="lg:w-[24px] lg:h-[24px] w-[96px] h-[96px] py-[4px] px-[1.54px] d-flex justify-content-center align-items-center">
+                <Link href="/"><svg className="lg:w-[21px] lg:h-[17px] w-[84px] h-[68px]" width="21" height="17" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M21.46 2.57434C20.69 2.92434 19.86 3.15434 19 3.26434C19.88 2.73434 20.56 1.89434 20.88 0.884341C20.05 1.38434 19.13 1.73434 18.16 1.93434C17.37 1.07434 16.26 0.574341 15 0.574341C12.65 0.574341 10.73 2.49434 10.73 4.86434C10.73 5.20434 10.77 5.53434 10.84 5.84434C7.28001 5.66434 4.11001 3.95434 2.00001 1.36434C1.63001 1.99434 1.42001 2.73434 1.42001 3.51434C1.42001 5.00434 2.17001 6.32434 3.33001 7.07434C2.62001 7.07434 1.96001 6.87434 1.38001 6.57434V6.60434C1.38001 8.68434 2.86001 10.4243 4.82001 10.8143C4.19074 10.9865 3.5301 11.0105 2.89001 10.8843C3.16162 11.7368 3.69355 12.4827 4.41103 13.0173C5.1285 13.5518 5.99546 13.848 6.89001 13.8643C5.37364 15.0648 3.494 15.7137 1.56001 15.7043C1.22001 15.7043 0.880009 15.6843 0.540009 15.6443C2.44001 16.8643 4.70001 17.5743 7.12001 17.5743C15 17.5743 19.33 11.0343 19.33 5.36434C19.33 5.17434 19.33 4.99434 19.32 4.80434C20.16 4.20434 20.88 3.44434 21.46 2.57434Z" fill="white"/>
                 </svg></Link>
               </div>
@@ -315,8 +315,8 @@ export default function Home() {
           </div>
 
           <div className="d-flex flex-column gap-[30px] pr-[30px] w-full">
-            <span className="fw-bold text-[33px]">Explore</span>
-            <div className="d-flex flex-column gap-[20px] font-[400] text-white">
+            <span className="fw-bold lg:text-[33px] text-[66px]">Explore</span>
+            <div className="d-flex lg:flex-col flex-rows lg:gap-[20px] gap-[40px] font-[400] lg:text-[16px] text-[32px]">
               <Link className="text-white link-underline link-underline-opacity-0" href="/">Homepage</Link>
               <Link className="text-white link-underline link-underline-opacity-0" href="/">For jobseekers</Link>
               <Link className="text-white link-underline link-underline-opacity-0" href="/">For clients</Link>
@@ -327,8 +327,8 @@ export default function Home() {
           </div>
 
           <div className="d-flex flex-column gap-[30px] pr-[30px] w-full">
-            <span className="fw-bold text-[33px]">Sectors</span>
-            <div className="d-flex flex-column gap-[20px] font-[400] ">
+            <span className="fw-bold lg:text-[33px] text-[66px]">Sectors</span>
+            <div className="d-flex lg:flex-col flex-rows lg:gap-[20px] gap-[40px] font-[400] lg:text-[16px] text-[32px]">
               <Link className="text-white link-underline link-underline-opacity-0" href="/">Software engineering</Link>
               <Link className="text-white link-underline link-underline-opacity-0" href="/">DevOps</Link>
               <Link className="text-white link-underline link-underline-opacity-0" href="/">Cloud</Link>
@@ -339,8 +339,8 @@ export default function Home() {
           </div>
 
           <div className="d-flex flex-column gap-[30px] pr-[30px] w-full">
-            <span className="fw-bold text-[33px]">Services</span>
-            <div className="d-flex flex-column gap-[20px] font-[400] ">
+            <span className="fw-bold lg:text-[33px] text-[66px]">Services</span>
+            <div className="d-flex lg:flex-col flex-rows lg:gap-[20px] gap-[40px] font-[400] lg:text-[16px] text-[32px]">
               <Link className="text-white link-underline link-underline-opacity-0" href="/">Nav item</Link>
               <Link className="text-white link-underline link-underline-opacity-0" href="/">Nav item</Link>
               <Link className="text-white link-underline link-underline-opacity-0" href="/">Nav item</Link>
